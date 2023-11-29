41c03fdf345685bc800e3764601eafd454424ab0
unknown <koundouras@gmail.com>
2023-11-29 09:38:14 +0200

    utility_classes

---
+++ user/week06/exercises/day01/utility_classes/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./*'],
+  purge:['./**/*.html'],
+  theme: {
+    extend: {    
+      colors:{
+        primary:'#ff0000',  
+      },
+      spacing:{
+        massive:'100rem',
+      },
+    },
+  },
+  plugins: [],
+}
